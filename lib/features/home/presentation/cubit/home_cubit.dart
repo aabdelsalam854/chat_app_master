3220c425fddc5110baba29041005603730650061
Ahmed <aabdelsalam854@gmail.com>
2025-04-19 19:16:23 +0200

    feat: Add conversation stream management in HomeCubit for real-time updates

--- lib/features/home/presentation/cubit/home_cubit.dart
+++ lib/features/home/presentation/cubit/home_cubit.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:chat_master/core/model/user_model.dart';
-import 'package:chat_master/features/chat/data/model/messages_model.dart';
+
 import 'package:chat_master/features/home/data/models/conversation.dart';
 import 'package:chat_master/features/home/domain/usecases/messages.dart';
 import 'package:equatable/equatable.dart';
@@ -10,7 +12,10 @@ part 'home_state.dart';
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit(this.usecases) : super(HomeInitial());
   final MessagesUsecases usecases;
-
+  final StreamController<List<Conversation>> _streamController =
+      StreamController<List<Conversation>>.broadcast();
+  Stream<List<Conversation>> get conversationsStream =>
+      _streamController.stream;
   Future<void> getUsers() async {
     emit(GetUsersLoadingState());
     var data = await usecases.getUsers('7SQ43pgmTqTgBeVKiflmyzPl4wp1');
@@ -26,14 +31,22 @@ class HomeCubit extends Cubit<HomeState> {
 
   Future<void> getAllConversations() async {
     emit(GetAllConversationsLoadingState());
+
     var data = await usecases.getAllConversations();
     data.fold(
       (failure) {
         emit(GetAllConversationsErrorState(failure.msg));
       },
-      (etAllConversations) {
-        emit(GetAllConversationsSuccessState(etAllConversations));
+      (conversations) {
+        _streamController.add(conversations);
+        emit(GetAllConversationsSuccessState(conversations));
       },
     );
+  }
+
+  @override
+  Future<void> close() {
+    _streamController.close();
+    return super.close();
   }
 }
